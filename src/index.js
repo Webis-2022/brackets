55e272c6422946fac7516a335f59ddf90942137a
Webis-2022 <verjovkin.jevgeni@gmail.com>
2024-09-29 08:12:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracketsMap = {};
+  let openBrackets = [];
+  let closeBrackets = new Set();
+
+  bracketsConfig.forEach(([open, close]) => {
+    bracketsMap[close] = open;
+    openBrackets.push(open);
+    closeBrackets.add(close);
+  });
+
+  for (let char of str) {
+    if (openBrackets.includes(char)) {
+      if (bracketsMap[char] === char) {
+        if (stack[stack.length - 1] === char) {
+          stack.pop(); 
+        } else {
+          stack.push(char); 
+        }
+      } else {
+        stack.push(char);
+      }
+    } 
+    else if (closeBrackets.has(char)) {
+      if (stack.length === 0 || stack.pop() !== bracketsMap[char]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
